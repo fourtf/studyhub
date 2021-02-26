ecb39d0eb895dfbcfd4dcca0a9fc89fb4e1d1cb6
fourtf <tf.four@gmail.com>
2021-02-26 21:04:14 +0100

    added basic http server

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/fourtf/studyhub
 
 go 1.15
+
+require github.com/go-chi/chi v1.5.3
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
-	fmt.Println("hello world")
-}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
 
+	log.Println("Listening at http://localhost:3001")
+
+	srv := http.Server{
+		Addr:         ":3001",
+		ReadTimeout:  1 * time.Minute,
+		WriteTimeout: 1 * time.Minute,
+		Handler:      r,
+	}
+
+	srv.ListenAndServe()
+}
